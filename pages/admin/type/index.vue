e74406b8e62cebe2b0babfa77501be224075129e
Sadeq Naderi <sadeq.naderi@gmail.com>
2025-04-03 18:57:35 +0330

    fix(admin panel): update admin layout

---
+++ pages/admin/type/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <admin-table />
+  </div>
+</template>
+
+<script>
+import AdminTable from "~/components/admin/AdminTable.vue";
+// import MyStock from "@/components/MyStock.vue";
+// import AnalysisReport from "@/components/AnalysisReport.vue";
+
+export default {
+  layout: "admin",
+  components: { AdminTable },
+  data() {
+    return {};
+  },
+};
+</script>
