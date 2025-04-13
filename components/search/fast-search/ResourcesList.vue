09a3867a4469d5f6c70d16d588070e24fd67eec8
Matin Jahi <Matin13822332@gmail.com>
2025-04-13 22:10:13 +0330

    feat(resources-list): add ResourcesList component for displaying a list of resources with icons and links

---
+++ components/search/fast-search/ResourcesList.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-list two-line>
+    <v-list-item v-for="(item, i) in resources" :key="i" class="pa-0">
+      <v-list-item-icon>
+        <img :src="require('@/assets/icons/' + item.icon)" />
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title class="gama-text-h3 font-weight-bold">
+          {{ item.title }}
+        </v-list-item-title>
+        <v-list-item-subtitle class="font-weight-medium primary-subtitle-500">
+          {{ item.link }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'ResourcesList',
+  props: {
+    resources: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.primary-subtitle-500 {
+  color: #2e90fa !important;
+  font-weight: 400 !important;
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: 0%;
+  vertical-align: middle;
+  line-height: 22px;
+}
+
+.v-application--is-ltr .v-list-item__icon:first-child {
+  margin-right: 12px;
+}
+</style> 
