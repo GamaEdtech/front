c2d65185ecbb57e6ef292caf6586f8f2824e8d4e
Matin Jahi <Matin13822332@gmail.com>
2025-04-13 22:11:22 +0330

    feat(fast-search): add FastSearchPage component and related exports for enhanced search functionality

---
+++ components/search/fast-search/index.js
@@ -0,0 +1,19 @@
+import LoadingOverlay from './LoadingOverlay.vue';
+import ResourcesList from './ResourcesList.vue';
+import TopicalTabs from './TopicalTabs.vue';
+import CustomDataTable from './CustomDataTable.vue';
+import TimelineColumn from './TimelineColumn.vue';
+import PageHeader from './PageHeader.vue';
+import FastSearchPage from './FastSearchPage.vue';
+
+export {
+  LoadingOverlay,
+  ResourcesList,
+  TopicalTabs,
+  CustomDataTable,
+  TimelineColumn,
+  PageHeader,
+  FastSearchPage
+};
+
+export default FastSearchPage; 
