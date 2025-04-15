0319a8e8b9c555f755de1bd5c0134ade7b672bc8
Matin Jahi <Matin13822332@gmail.com>
2025-04-15 10:15:01 +0330

    feat(fast-search): enhance FastSearchPage component with responsive layout for mobile and desktop views

--- components/search/fast-search/FastSearchPage.vue
+++ components/search/fast-search/FastSearchPage.vue
@@ -4,15 +4,31 @@
     <loading-overlay :is-loading="isLoading" />
 
     <!-- Header Section with Breadcrumb and Title -->
-    <page-header :breads="breads" :book-image="bookImage" :view-count="viewCount">
-      <template v-slot:content>
-        <!-- Resources List -->
-        <resources-list :resources="resources" />
-        
-        <!-- Topical Tabs -->
-        <topical-tabs :papers="papers" />
+    <page-header
+      :breads="breads"
+      :book-image="bookImage"
+      :view-count="viewCount"
+    >
+      <!-- Desktop view -->
+      <template slot="content">
+        <div class="d-none d-md-block">
+          <!-- Resources List -->
+          <resources-list :resources="resources" />
+
+          <!-- Topical Tabs -->
+          <topical-tabs :papers="papers" />
+        </div>
       </template>
     </page-header>
+
+    <!-- Mobile view -->
+    <div class="d-block d-md-none mt-4">
+      <!-- Resources List -->
+      <resources-list :resources="resources" />
+
+      <!-- Topical Tabs -->
+      <topical-tabs :papers="papers" />
+    </div>
 
     <!-- Table & Timeline -->
     <v-row class="d-flex flex-row align-start justify-space-between">
@@ -22,13 +38,16 @@
           type="table"
           class="elevation-0 custom-table"
         ></v-skeleton-loader>
-        <custom-data-table 
+        <custom-data-table
           v-else
           :headers="headers"
           :items="combinedPaperItems"
         />
       </v-col>
-      <v-col cols="2" class="timeline-column d-flex flex-column align-center">
+      <v-col
+        cols="2"
+        class="timeline-column d-none d-md-flex flex-column align-center"
+      >
         <template v-if="isLoading">
           <v-skeleton-loader
             type="list-item-three-line, list-item-three-line"
@@ -45,61 +64,61 @@
 </template>
 
 <script>
-import LoadingOverlay from './LoadingOverlay.vue';
-import ResourcesList from './ResourcesList.vue';
-import TopicalTabs from './TopicalTabs.vue';
-import CustomDataTable from './CustomDataTable.vue';
-import TimelineColumn from './TimelineColumn.vue';
-import PageHeader from './PageHeader.vue';
+import CustomDataTable from "./CustomDataTable.vue";
+import LoadingOverlay from "./LoadingOverlay.vue";
+import PageHeader from "./PageHeader.vue";
+import ResourcesList from "./ResourcesList.vue";
+import TimelineColumn from "./TimelineColumn.vue";
+import TopicalTabs from "./TopicalTabs.vue";
 
 export default {
-  name: 'FastSearchPage',
+  name: "FastSearchPage",
   components: {
     LoadingOverlay,
     ResourcesList,
     TopicalTabs,
     CustomDataTable,
     TimelineColumn,
-    PageHeader
+    PageHeader,
   },
   props: {
     // Pass down all props that can be customized
     isLoading: {
       type: Boolean,
-      default: false
+      default: false,
     },
     bookImage: {
       type: String,
-      default: require('@/assets/images/BiologyBook.png')
+      default: require("@/assets/images/BiologyBook.png"),
     },
     breads: {
       type: Array,
-      required: true
+      required: true,
     },
     viewCount: {
       type: [String, Number],
-      default: '28k'
+      default: "28k",
     },
     resources: {
       type: Array,
-      required: true
+      required: true,
     },
     papers: {
       type: Object,
-      required: true
+      required: true,
     },
     headers: {
       type: Array,
-      required: true
+      required: true,
     },
     combinedPaperItems: {
       type: Array,
-      required: true
+      required: true,
     },
     timeline: {
       type: Array,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+};
 </script>
