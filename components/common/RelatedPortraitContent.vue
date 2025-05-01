d7685c8f6f88ad129be05edf38e108d9eebfb81f
Danial <danial.slriii@gmail.com>
2025-05-01 17:32:07 +0330

    fix(relatedslider): fix next and prec buttons

--- components/common/RelatedPortraitContent.vue
+++ components/common/RelatedPortraitContent.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="mx-4">
-        <h1>Related {{ pageName }} </h1>
-        <v-slide-group>
+        <div class="d-flex mb-4" style="gap: 8px;">
+            <div style="background-color: #2E90FA; width: 20px; height: 20px; display: flex;justify-content: center; align-items: center; border-radius: 50%;">
+                <span style="color: white;" class="icon icon-paper"></span>
+            </div>
+            
+            <h1 style="font-size:14px !important;font-weight: 600;color: #344054;">Related {{ pageName }} </h1>
+        </div>
+        <v-slide-group show-arrows >
             <v-slide-item
                 v-for="item in data"
                 :key="item.id"
@@ -60,4 +66,65 @@ export default {
 
 </script>
 <style>
+
+.v-slide-group {
+  display: flex !important;
+  flex-direction: column !important;
+  align-items: center; 
+}
+.v-slide-group:hover > .v-slide-group__prev,
+.v-slide-group:hover > .v-slide-group__next {
+    opacity: 1;
+    visibility: visible;
+}
+
+.v-slide-group__wrapper {
+  order: 1;
+  width: 100%;
+  height: 260px;
+  align-items: center;
+}
+
+.v-slide-group__prev,
+.v-slide-group__next {
+  order: 2;
+  margin-top: 12px;
+  background-color: #344054CC;
+  border-radius: 50%;
+  width: 40px !important;
+  height: 40px !important;
+  min-width: 40px !important;
+  border: 1px solid #E4E7EC;
+  color: white;
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 300ms ease, visibility 300ms ease;
+}
+.v-slide-group__prev > i,
+.v-slide-group__next > i{
+    color:white !important;
+}
+
+.v-slide-group__prev > .theme--light.v-icon.v-icon.v-icon--disabled ,
+.v-slide-group__next > .theme--light.v-icon.v-icon.v-icon--disabled {
+    color:rgba(255, 255, 255, 0.544) !important;
+}
+
+.v-slide-group__prev > .v-slide-group__prev--disabled, .v-slide-group__prev--disabled,
+.v-slide-group__next > .v-slide-group__next--disabled, .v-slide-group__next--disabled {
+    pointer-events: unset !important;
+}
+
+.v-slide-group__prev {
+    position: absolute;
+    margin-top: 90px;
+    left: 15px;
+    z-index: 10;
+}
+.v-slide-group__next {
+    position: absolute;
+    margin-top: 90px;
+    right: 15px;
+    z-index: 10;
+}
 </style>
