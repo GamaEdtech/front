de85d7f372a09b0537092d45a4e7d548f94fd14b
Matin Jahi <Matin13822332@gmail.com>
2025-06-03 19:51:23 +0330

    feat(testMathJax): create a new component for MathJax integration with a default formula for Testing Purpose

---
+++ pages/testMathJax.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mt-10">
+    <textarea v-model="formula"/>
+    <vue-mathjax :formula="formula"/>
+  </div>
+  <Script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS_HTML"></Script>
+</template>
+
+<script setup lang="ts">
+const formula = ref('$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$');
+</script>
