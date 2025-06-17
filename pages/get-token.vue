2d0394a09cb4afe6ca5a97d7e9a10cd120cae5ae
Hilsonxhero <hilsonxhero.me@gmail.com>
2025-06-17 13:08:54 +0330

    feat (get-token) : add get-token page

---
+++ pages/get-token.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <section>
+      <token-hero-section />
+    </section>
+    <section>
+      <token-about-section />
+    </section>
+    <section>
+      <token-how-get-token-section />
+    </section>
+    <section>
+      <token-earn-get-section />
+    </section>
+    <section>
+      <token-trust-community-section />
+    </section>
+    <section>
+      <token-tokenomics-section />
+    </section>
+    <section>
+      <token-profit-buyback-section />
+    </section>
+    <section>
+      <token-purchase />
+    </section>
+    <section>
+      <token-how-buy-section />
+    </section>
+    <section>
+      <token-faq />
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
