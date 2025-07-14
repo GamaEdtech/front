5cb5a83b872b57e5e46b567d385351954dd8c443
Matin Jahi <Matin13822332@gmail.com>
2025-07-14 17:46:57 +0330

    feat(SchoolChips): implement query parameter handling on chip click events

--- pages/school/[id]/[[slug]].vue
+++ pages/school/[id]/[[slug]].vue
@@ -146,7 +146,7 @@
         </v-row>
         <v-row>
           <v-col cols="12" md="8">
-            <school-detail-school-chips :contentData="contentData" />
+            <school-detail-school-chips :contentData="contentData" @onChipsClick="handleQueryParameters" />
             <school-detail-tuition-info :contentData="contentData" />
             <school-detail-facilities
               :facilities="contentData.tags"
@@ -187,6 +187,8 @@
 </template>
 
 <script setup>
+import { useRouter, useRoute } from 'nuxt/app';
+
 const route = useRoute();
 const router = useRouter();
 const tourPanoramas = ref([]);
@@ -365,6 +367,18 @@ function handleLocationUpdate(locationData) {
 function handleCommentSubmitted() {
   // Refresh comments when a new comment is submitted
   refreshComments();
+}
+
+function handleQueryParameters(data){
+  const query = {};
+  if (data.countryId) query.country = data.countryId;
+  if (data.type === 'state' || data.type === 'city') {
+    if (data.stateId) query.state = data.stateId;
+  }
+  if (data.type === 'city') {
+    if (data.cityId) query.city = data.cityId;
+  }
+  router.push({ path: '/school', query });
 }
 
 
