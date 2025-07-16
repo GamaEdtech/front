ba669d30a5e0126be5560461091158bdcbf51300
alireza013013 <alireza.abdollahi19998@gmail.com>
2025-07-16 21:50:09 +0330

    feat(school):add new sort option lastModifyDate

--- pages/school/index.vue
+++ pages/school/index.vue
@@ -133,6 +133,10 @@ const isMobile = ref(display.xs);
 
 const sortList = [
   {
+    value: "lastModifyDate",
+    title: "Recently Updated",
+  },
+  {
     value: "score",
     title: "Highest score",
   },
