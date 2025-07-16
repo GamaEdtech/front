2bbeb0e1776592fb58e86fec8edd98cc36201aaf
alireza013013 <alireza.abdollahi19998@gmail.com>
2025-07-16 15:45:49 +0330

    fix(pwa): change gloabal pattern

--- nuxt.config.js
+++ nuxt.config.js
@@ -178,7 +178,7 @@ export default defineNuxtConfig({
       name: 'Gamatrain'
     },
     injectManifest: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      globPatterns: [],
       swSrc: 'public/sw.js'
     },
     workbox: {
